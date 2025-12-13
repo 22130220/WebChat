3392157aed02c49eceb5bed29a91d18dbcf4b9b0
22130220 <22130220@st.hcmuaf.edu.vn>
2025-12-13 12:56:55 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./stores/counterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button
+        className="p-2 bg-amber-600"
+        onClick={() => dispatch(increment())}
+      >
+        +
+      </button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default App;
